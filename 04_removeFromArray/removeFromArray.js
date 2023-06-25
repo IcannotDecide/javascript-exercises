a8e2867d7c31d3449d827287a61dd1a89501cd90
IcannotDecide <matthewat43@gmail.com>
2023-06-25 14:19:03 +0100

    Solve 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let newArr = arr;
+    
+    for (let i = 0; i < newArr.length; i++) {
+        for (let  j = 0; j < remove.length; j++) {
+            if (newArr[i] === remove[j]) {
+                newArr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (typeof start !== "number" || typeof end !== "number" || start < 0 || end < 0) return "ERROR";
+
+    let nums = [start, end].sort((a, b) => a - b)
+
+    let sum = 0;
+
+    for (let i = nums[0]; i <= nums[1]; i++) sum +=i;
+
+    return sum
 };
 
 // Do not edit below this line
