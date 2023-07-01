d92b41c64091493416517746a95f4fc8d28f38ee
IcannotDecide <matthewat43@gmail.com>
2023-07-01 16:27:16 +0100

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let arr = [1]
+    let oldNum = 1;
+    let newNum = 1;
+    let temp;
 
+    if (+num > 0) {
+        while (arr.length < num) {
+            arr.push(newNum);
+            temp = newNum;
+            newNum += oldNum;
+            oldNum = temp;
+        }
+      return arr[num-1]
+    }
+    return "OOPS"
 };
 
 // Do not edit below this line
