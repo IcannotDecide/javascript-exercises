94efac656e27f07eadebc286b441d9c1b478de65
IcannotDecide <matthewat43@gmail.com>
2023-07-01 16:04:59 +0100

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[^abcdefghiklmnopqrstuvwxyz0123456789]/gi, "").toLowerCase();
+    if (newStr === newStr.split("").reverse().join("")) return true;
+    return false;
 };
 
 // Do not edit below this line
