0a312ec0a8ea20281177e3f641dd02fd215a3847
IcannotDecide <matthewat43@gmail.com>
2023-07-01 15:29:23 +0100

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, item) => {
+    return total + item
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...theArgs) {
+  var total = 1;
+  for (const arg of theArgs) {
+    total *= arg
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    console.log(total, i)
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
